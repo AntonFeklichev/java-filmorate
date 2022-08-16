5279c6f624e7aa44438e35d89219f57a035a6c99
Matvey Smorodin <mat.smooooder@gmail.com>
2022-08-16 14:55:02 +0300

    add film-controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,2 +1,32 @@
-package ru.yandex.practicum.filmorate.controller;public class FilmController {
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+    private Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film createFilm(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateUser(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
 }
