2576280bb9c8e11f5e4ab0f2a967bbf7ed8ca171
Matvey Smorodin <mat.smooooder@gmail.com>
2022-08-18 15:51:11 +0300

    add film validation

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,16 +1,34 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.Valid;
+import javax.validation.ValidationException;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private static final LocalDate FIRST_MOVIE_RELEASE_DATE = LocalDate.of(1895, Month.DECEMBER, 28);
     private Map<Integer, Film> films = new HashMap<>();
+    private int idForNewFilms = 1;
+
+    private void addNewFilm(Film film) {
+        generateAndSetId(film);
+        films.put(film.getId(), film);
+    }
+
+    private void generateAndSetId(Film film) {
+        film.setId(idForNewFilms);
+        idForNewFilms++;
+    }
 
     @GetMapping
     public Collection<Film> getFilms() {
@@ -18,14 +36,20 @@ public class FilmController {
     }
 
     @PostMapping
-    public Film createFilm(@RequestBody Film film) {
-        films.put(film.getId(), film);
+    public Film createFilm(@Valid @RequestBody Film film) {
+        addNewFilm(film);
+        log.info("{} was added", film);
         return film;
     }
 
     @PutMapping
-    public Film updateUser(@RequestBody Film film) {
-        films.put(film.getId(), film);
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        if (films.containsKey(film.getId())) {
+            films.put(film.getId(), film);
+            log.info("{} was updated", film);
+        } else {
+            throw new ValidationException("Invalid film id for updating");
+        }
         return film;
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,13 +2,23 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
+    @Email
+    @NotBlank
     private String email;
+    @NotBlank
     private String login;
+    @NotBlank
     private String name;
+    @Past
+    @NotNull
     private LocalDate birthday;
 }
