3b2246f969c03df93c2c9c10f9302c21a3b3ef92
Matvey Smorodin <mat.smooooder@gmail.com>
2022-09-02 17:55:55 +0300

    feat: Add in-memory UserStorage implementation

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,62 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static ru.yandex.practicum.filmorate.validator.IdForUpdatingValidator.validateIdForUpdating;
+
+@Slf4j
+
+public class InMemoryUserStorage implements UserStorage {
+    private static int idForNewUsers = 1;
+    private Map<Integer, User> users = new HashMap<>();
+
+    @Override
+    public User addUser(User user) {
+        generateAndSetId(user);
+        setNameToLoginIfNameIsEmpty(user);
+        users.put(user.getId(), user);
+        log.info("{} was added", user);
+        return user;
+    }
+
+    private void generateAndSetId(User user) {
+        user.setId(idForNewUsers);
+        idForNewUsers++;
+        log.trace("id = {} was generated", user.getId());
+    }
+
+    private void setNameToLoginIfNameIsEmpty(User user) {
+        String name = user.getName();
+        String login = user.getLogin();
+        if (name.isBlank()) {
+            user.setName(login);
+            log.trace("name was blank so login was used as name");
+        }
+    }
+
+    @Override
+    public User deleteUser(User user) {
+        users.remove(user.getId());
+        return user;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        setNameToLoginIfNameIsEmpty(user);
+        validateIdForUpdating(user.getId(), users.keySet());
+        users.put(user.getId(), user);
+        log.info("{} was updated");
+        return user;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return new ArrayList<User>(users.values());
+    }
+}
