f2179f0db15cceff1f8c44eac082434f6c323d39
Matvey Smorodin <mat.smooooder@gmail.com>
2022-09-05 20:37:49 +0300

    feat: Add unknown user handling

--- src/main/java/ru/yandex/practicum/filmorate/validator/IdValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/IdValidator.java
@@ -1,11 +1,15 @@
 package ru.yandex.practicum.filmorate.validator;
 
+import ru.yandex.practicum.filmorate.exception.UnknownUserException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.util.Collection;
 
 public class IdValidator {
-    public static void validateUserId(int id, Collection<Integer> existingId) throws ValidationException {
-        if (!existingId.contains(id)) throw new ValidationException("Unknown user");
+    public static void validateUserId(int id, Collection<Integer> existingId) throws UnknownUserException {
+        String msg = String.format("Unknown user: %d", id);
+        if (!existingId.contains(id)) throw new UnknownUserException(msg);
     }
+//
+//    public static void validateFilmId(int id, Collection<Integer> existingId) throws
 }
