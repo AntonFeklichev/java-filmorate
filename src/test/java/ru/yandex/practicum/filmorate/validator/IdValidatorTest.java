27fbb1aa8bb54d40703b9cc2cb451f9a777265c7
Matvey Smorodin <mat.smooooder@gmail.com>
2022-09-07 14:26:05 +0300

    test: Remove irrelevant tests and refactor id validation tests

--- src/test/java/ru/yandex/practicum/filmorate/validator/IdValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validator/IdValidatorTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.validator;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.exception.UnknownUserException;
 
 import java.util.Set;
 
@@ -10,18 +10,18 @@ public class IdValidatorTest {
     private final Set<Integer> setOfId = Set.of(1, 2, 3, 4, 5);
 
     @Test
-    public void shouldNotThrowValidationExceptionWhenUpdatingExistingId() {
+    public void shouldNotThrowExceptionWhenUpdatingExistingId() {
         final int validIdForUpdating = 4;
         Assertions.assertDoesNotThrow(() -> IdValidator.validateUserId(validIdForUpdating, setOfId),
-                "ValidationException is thrown but id is correct");
+                "Exception is thrown but id is correct");
     }
 
     @Test
-    public void shouldThrowValidationExceptionWhenUpdatingUnknownId() {
+    public void shouldThrowUnknownUserException() {
         final int invalidIdForUpdating = 10;
-        Exception ex = Assertions.assertThrows(ValidationException.class,
+        Exception ex = Assertions.assertThrows(UnknownUserException.class,
                 () -> IdValidator.validateUserId(invalidIdForUpdating, setOfId),
-                "ValidationException is not thrown but id is not correct");
-        Assertions.assertEquals("Invalid id for updating", ex.getMessage());
+                "UnknownUserException is not thrown but id is not correct");
+        Assertions.assertEquals(String.format("Unknown user: %d", invalidIdForUpdating), ex.getMessage());
     }
 }
